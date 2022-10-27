7db1616f33bafc598ae0abdd18b2c5ce804f9a25
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-10-26 21:42:58 -0600

    fixed ID bug for intern and engineer

--- dist/renderHtml.js
+++ dist/renderHtml.js
@@ -30,7 +30,7 @@ const createTeam = team => {
         </div>
         <div class="card-body">
             <ul class="list-group">
-                <li class = "list-group-item">ID: ${engineer.getRole()}</li>
+                <li class = "list-group-item">ID: ${engineer.getId()}</li>
                 <li class = "list-group-item">Email: <a href="mailto:${engineer.getEmail()}"> ${engineer.getEmail()}</a></li>
                 <li class = "list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()} target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
             </ul> 
@@ -49,7 +49,7 @@ const createTeam = team => {
         </div>
         <div class="card-body">
             <ul class="list-group">
-                <li class = "list-group-item">ID: ${intern.getRole()}</li>
+                <li class = "list-group-item">ID: ${intern.getId()}</li>
                 <li class = "list-group-item">Email: <a href="mailto:${intern.getEmail()}"> ${intern.getEmail()}</a></li>
                 <li class = "list-group-item">School:${intern.getSchool()}</li>
             </ul> 
